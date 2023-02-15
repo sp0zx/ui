1a4af03b8b3ed3bb1b53a92f30c504badb20ac6c
sp0zx <122081930+sp0zx@users.noreply.github.com>
2023-02-15 18:23:02 -0500

    Update and rename whitelist.lua to ex.lua

---
+++ ex.lua
@@ -0,0 +1,55 @@
+
+local StarId = {
+	[2314260796] = true,
+	[3553041817] = true,
+}
+
+local CrownId = {
+[1865110551] = true,
+[87026572] = true,
+}
+
+local DiamondId = {
+	[3979423295] = true, -- anthony
+	[69088811] = true, -- you make
+}
+
+local EnemyId = {} 
+
+function premium()
+	for _,v in pairs(game:GetService('Players'):GetChildren()) do
+		if StarId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
+				end
+			end
+		elseif CrownId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]'..v.DisplayName)
+				end
+			end
+		elseif DiamondId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]'..v.DisplayName)
+				end
+			end
+		elseif EnemyId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('🤡 Kill This Nigger 🤡')
+				end
+			end
+		elseif v.Character then
+			if v.Character.Parent.Name == 'Players' then
+				if not v.Character.UpperTorso:FindFirstChild('BodyBackAttachment') then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🙂]'..v.DisplayName)
+				end
+			end
+		end
+	end
+end
+local success,err = pcall(premium)
+return StarId
